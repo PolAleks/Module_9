824bde29c5b00dc5f3a6ef0f6b0b341ec089347b
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 17:31:13 +0300

    Делегат на метод возвращающий раницу двух чисел

---
+++ Task_9.3.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_9._3._2
+{
+    internal class Program
+    {
+        static int Differece (int a, int b)
+        {
+            return b - a;
+        }
+
+        public delegate int Result( int x, int y );
+
+        static void Main(string[] args)
+        {
+            Result result = Differece;
+            Console.WriteLine(result.Invoke(10, 20));
+
+        }
+    }
+}
