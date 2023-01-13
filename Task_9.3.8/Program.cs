f6998f8a050a6d508acab114ea67f9a2d9fbbb8d
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 18:08:55 +0300

    Применение шаблонных делегатов

---
+++ Task_9.3.8/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_9._3._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action showMessage = ShowMessage;
+            showMessage();
+
+            Func<int, int, int, int> sum = Sum;
+            int result = sum.Invoke(10, 20, 30);
+            Console.WriteLine(result);
+
+            Predicate<string> checkLength = CheckLength;
+            Console.WriteLine(checkLength.Invoke("Hell0"));
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Привет МИР!");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3)
+                return true;
+            return false;
+        }
+    }
+}
