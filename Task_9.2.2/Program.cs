15da2763554663aedd97873423f941ab0b93923c
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 14:08:12 +0300

    Обработка исключения ArgumentOutOfRangeException

---
+++ Task_9.2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_9._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException("Былли введены некорректные данные!");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.ParamName);
+            }
+            finally
+            {
+                Console.WriteLine("Работа приложения завершена!");
+            }
+        }
+    }
+}
