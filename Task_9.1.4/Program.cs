5073c1156870308bc7a485ce6b7503a0b6858e96
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 13:36:12 +0300

    Переопределение свойств Message и HelpLink

---
+++ Task_9.1.4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task_9._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exeption = new Exception("Какая-то ошибка");
+            exeption.HelpLink = "https://ya.ru";
+        }
+    }
+}
