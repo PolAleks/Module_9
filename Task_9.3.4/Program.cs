e1e1b085eb709d8e8524ef59f53b8ade4c1fa92b
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 17:41:30 +0300

    Создание многоадрессного делегата

---
+++ Task_9.3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_9._3._4
+{
+    internal class Program
+    {
+        delegate void Calculate(int a, int b);
+        static void Main(string[] args)
+        {
+            Calculate calc = Difference;
+            calc += Sum;
+
+            calc.Invoke(10, 20);
+        }
+
+        static void Difference (int a, int b)
+        {
+            int c = b - a;
+            Console.WriteLine(c);
+        }
+
+        static void Sum (int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
