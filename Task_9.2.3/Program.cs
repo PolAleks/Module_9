7b16888b68540060c771e23b5f10ca23c9d9fa02
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-13 14:12:36 +0300

    Обработка исключения RankException

---
+++ Task_9.2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_9._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException("Ошибка!");
+            }
+            catch(RankException e)
+            {
+                Console.WriteLine(e.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("Конец программы!");
+            }
+        }
+    }
+}
