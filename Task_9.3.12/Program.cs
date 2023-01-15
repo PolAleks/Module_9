81553692c7cd29c38dbf37d01302e37f7e8b9666
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-15 10:05:41 +0300

    Реализация анонимного делегата

---
+++ Task_9.3.12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_9._3._12
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate(string name);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate smd = delegate (string _name)
+            {
+                Console.WriteLine(_name);
+            };
+            smd.Invoke("Hello world!");
+        }
+    }
+}
