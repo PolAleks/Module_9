912ec400a0d1748862f534f68b070a1b663c77b9
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-15 10:11:34 +0300

    Еще один анонимный метод

---
+++ Task_9.3.13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_9._3._13
+{
+    internal class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate rnd = delegate()
+            {
+                return new Random().Next(0, 100);
+            };
+            Console.WriteLine(rnd.Invoke());
+        }
+    }
+}
